690ebc359ca5f0e4881effd4cecb9a7470a87a82
Juan Ignacio Salerno <juan.salerno@globant.com>
2020-06-03 09:49:11 -0300

    Finishing JS advanced module with challenges completed

--- week-1/advanced-JS/script.js
+++ week-1/advanced-JS/script.js
@@ -52,12 +52,12 @@ let jane = Object.create(personProto, {
 // Primitives vs. Objects
 
 // Primitives
-let a = 23;
+/* let a = 23;
 let b = a;
 a = 46;
 console.log(a);
 console.log(b);
-
+ */
 /* 
 // Objects
 let obj1 = {
@@ -184,6 +184,247 @@ game();
     console.log(score >= 5 - goodLuck);
 })(5);
 */
+
+// Closures
+/* 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    var actual = 2020
+    return function(yearOfBirth) {
+        var age = actual - yearOfBirth;
+        console.log((retirementAge - age) + a );
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+retirement(66)(1990);
+
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John'); */
+
+// Lecture: Bind, call and apply
+/*
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+// Another cool example
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
+*/
+
+// CHALLENGE:
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) 
+(Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as 
+you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private 
+and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+/* (function () {
+    function Question(id, question, answers, correctAnswer) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    } 
+    
+    Question.prototype.logQuestion = function() {
+        console.log(this.id + '. ' + this.question)
+        for(let i=0; i < this.answers.length; i++) {
+            console.log(i + '. ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.logResult = function(chosenNumber) {
+        if(chosenNumber === this.correctAnswer) {
+            console.log('Correct Answer!');
+            
+        } else {
+            console.log('Wrong answer. Try Again');
+        }
+    }
+    
+    Question.prototype.logScore = function(score) {
+        console.log('Your current score: ' + score);
+    }
+    
+    let q1 = new Question(1, 'What is my name?', ['Pedro', 'Juan', 'Lucas'], 1)
+    let q2 = new Question(2, 'Where do I live?', ['Tandil', 'Boston', 'San Francisco'], 0)
+    let q3 = new Question(3, 'What is the course about?', ['Java', 'Python', 'JavaScript'], 2)
+    
+    let questions = [q1, q2, q3];
+    
+    randomQ = Math.floor(Math.random() * questions.length);
+    
+    questions[randomQ].logQuestion();
+        
+    let chosenInput = parseInt(prompt('Please select the correct answer (just type the number). Or type exit to quit'));
+
+    questions[randomQ].logResult(chosenInput);
+})() 
+*/
+
+// --> expert level
+(function () {
+    function Question(id, question, answers, correctAnswer) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    } 
+    
+    Question.prototype.logQuestion = function() {
+        console.log(this.id + '. ' + this.question)
+        for(let i=0; i < this.answers.length; i++) {
+            console.log(i + '. ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.logResult = function(chosenNumber, callback) {
+        let sc;
+
+        if(chosenNumber === this.correctAnswer) {
+            console.log('Correct Answer!');
+            sc = callback(true)
+        } else {
+            console.log('Wrong answer. Try Again');
+            sc = callback(false);
+        }
+        this.logScore(sc)
+    }
+    
+    Question.prototype.logScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('-------------------');
+    }
+    
+    let q1 = new Question(1, 'What is my name?', ['Pedro', 'Juan', 'Lucas'], 1)
+    let q2 = new Question(2, 'Where do I live?', ['Tandil', 'Boston', 'San Francisco'], 0)
+    let q3 = new Question(3, 'What is the course about?', ['Java', 'Python', 'JavaScript'], 2)
+    
+    let questions = [q1, q2, q3];
+
+    function score() {
+        let score = 0;
+        return function(correct) {
+            if(correct) {
+                score++;
+            }
+            return score
+        }
+    }
+
+    let keepScore = score();
+
+    function next() {
+              
+        randomQ = Math.floor(Math.random() * questions.length);
+        
+        questions[randomQ].logQuestion();
+            
+        let chosenInput = prompt('Please select the correct answer (just type the number). Or type exit to quit');
+    
+        if(chosenInput !== 'exit') {
+
+            questions[randomQ].logResult(parseInt(chosenInput), keepScore);
+            next()
+        }
+    }
+
+    next()
+})()
+
+
+
+
+
 
 
 
