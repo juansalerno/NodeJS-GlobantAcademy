6df2985d1582bf14b61595304476913e30e989a8
Juan Ignacio Salerno <juan.salerno@globant.com>
2020-06-03 15:52:31 -0300

    Budget-App: Updating the Budget UI Controller

--- week-1/Budget-app/app.js
+++ week-1/Budget-app/app.js
@@ -103,7 +103,11 @@ let UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
 
     return {
@@ -148,6 +152,19 @@ let UIController = (function () {
 
             fieldsArray[0].focus();
         },
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.porcentage;
+
+            if(obj.porcentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.porcentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---'
+            }
+
+        },
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -181,7 +198,7 @@ let controller = (function (budgetCtrl, UICtrl) {
         let budget = budgetCtrl.getBudget();
 
         // 3. display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
         
     }
 
@@ -212,7 +229,12 @@ let controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('App has started.');
-
+            UICtrl.displayBudget({
+                budget: 0,
+                totalIncome: 0,
+                totalExp: 0,
+                porcentage: -1
+            });
             setupEventListeners();
         }
     }
