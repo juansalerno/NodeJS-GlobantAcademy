780d759522b884611f7678b6d4a48fa6b848bb75
Juan Ignacio Salerno <juan.salerno@globant.com>
2020-06-05 18:09:13 -0300

    Forkify-App: Building the Search model and controller

--- week-1/Forkify-app/src/js/index.js
+++ week-1/Forkify-app/src/js/index.js
@@ -1,4 +1,41 @@
 // Global app controller
-import num from './test';
 
-console.log(`I imported ${num} from another module called test!`)
+import Search from './models/Search';
+
+/* Global state of the app:
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+
+    // 1. get the query from view
+    const query = 'pizza' // to do
+
+    if(query) {
+        // 2. new search object and add to state
+        state.search = new Search(query);
+
+        // 3. prepare UI for results
+
+        // 4. search for recipes
+        await state.search.getResults();
+
+        // 5. render results on UI
+        console.log(state.search.result)
+
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
+/* const search = new Search('pizza');
+console.log(search);
+search.getResults(); */
