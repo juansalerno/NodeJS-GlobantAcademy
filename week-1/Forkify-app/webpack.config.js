9067ec3b2c39837d46e93b8ed5fde9b080ab599c
Juan Ignacio Salerno <juan.salerno@globant.com>
2020-06-05 17:02:20 -0300

    Forkify-App: config settings (Babel)

--- week-1/Forkify-app/webpack.config.js
+++ week-1/Forkify-app/webpack.config.js
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+                exclude: /node_modules/, // files to be ignored
+                use: {
+                    loader: 'babel-loader' // specify the loader
+                }
+            }
+        ]
+    }
 }
