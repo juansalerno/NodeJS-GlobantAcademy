8fcc60e20f32548332842ea47b99924b120f2076
Juan Ignacio Salerno <juan.salerno@globant.com>
2020-06-16 16:23:28 -0300

    Task-App: Logging in Users

--- week-3/task-manager/src/models/user.js
+++ week-3/task-manager/src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -41,14 +42,30 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// a method set in Schema.statics is for access directly from the model
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) { // standard function is required because the "this" binding plays an important role
     const user = this
 
     if (user.isModified('password')) { // method provided by mongoose
         user.password = await bcrypt.hash(user.password, 8)
-    }
-
-    console.log('just before saving')    
+    }   
 
     next()
 })
